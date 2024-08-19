b1feab8426bde742471b3198280ec3a76c06c03a
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2024-08-19 15:06:02 +0200

    chore: Initial commit

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.10
+import PackageDescription
+
+let package = Package(
+    name: "InfomaniakDeviceCheck",
+    platforms: [.iOS(.v14), .macOS(.v11)],
+    products: [
+        .library(
+            name: "InfomaniakDeviceCheck",
+            targets: ["InfomaniakDeviceCheck"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "InfomaniakDeviceCheck"
+        ),
+        .testTarget(
+            name: "InfomaniakDeviceCheckTests",
+            dependencies: ["InfomaniakDeviceCheck"]
+        ),
+    ]
+)
