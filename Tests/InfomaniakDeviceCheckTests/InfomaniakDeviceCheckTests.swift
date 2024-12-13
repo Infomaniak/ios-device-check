6f73d5bcd3be969ab24e273618c9abcb6810e536
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2024-12-13 07:44:22 +0100

    test: Update test env

--- Tests/InfomaniakDeviceCheckTests/InfomaniakDeviceCheckTests.swift
+++ Tests/InfomaniakDeviceCheckTests/InfomaniakDeviceCheckTests.swift
@@ -3,9 +3,10 @@ import XCTest
 
 final class InfomaniakDeviceCheckTests: XCTestCase {
     func testAttestationToken() async throws {
-        let attestation = try await InfomaniakDeviceCheck().generateAttestationFor(
+        let attestation = try await InfomaniakDeviceCheck(environment: .preprod).generateAttestationFor(
             targetUrl: URL(string: "https://login.infomaniak.com")!,
-            bundleId: "com.infomaniak.mail"
+            bundleId: "com.infomaniak.mail",
+            bypassValidation: true
         )
     }
 }
